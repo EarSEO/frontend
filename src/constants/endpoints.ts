4aa69e27a970c9503798caeed0b6f914e4ef4b74
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-12-01 04:11:35 +0900

    [#5] feat : 경로 시작/중지, 도슨트 조회 API 엔드포인트 설정

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -10,6 +10,16 @@ export const API_ENDPOINTS = {
     EMAIL_VERIFY: "/api/member/email/verify",
     NICKNAME_CHECK: "/api/member/nickname/check",
   },
+  SIGHT: {
+    DOCENT_SCRIPT: "/api/sight/docent",
+  },
+  STORY: {
+    DOCENT_SCRIPT: "/api/story/docent",
+  },
+  ROUTE: {
+    CREATE_IN_PROGRESS_ROUTE: "/api/user/route/in-progress",
+    COMPLETE_ROUTE: (routeId: number) => `/api/user/route/${routeId}'complete`,
+  },
 };
 
 export default API_ENDPOINTS;
