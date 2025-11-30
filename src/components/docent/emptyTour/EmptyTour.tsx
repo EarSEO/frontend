8274bb175f75a6e09a2af9e3e4e413db921583ab
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-12-01 04:10:19 +0900

    [#5] feat : 빈 경로 안내 페이지 구현

---
+++ src/components/docent/emptyTour/EmptyTour.tsx
@@ -0,0 +1,36 @@
+import { Animated, Text } from "react-native";
+
+import styled from "styled-components/native";
+
+import GoGetCourseButton from "@/components/docent/emptyTour/GoGetCourseButton";
+
+import { theme } from "@/styles/theme";
+import View = Animated.View;
+
+const EmptyTour: React.FC = () => {
+  const noCourseText = "선택된 경로가 없습니다.";
+  return (
+    <Container>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text
+          style={{
+            fontSize: theme.typography.fontSize.lg,
+            fontFamily: theme.typography.fontFamily.medium,
+            color: theme.colors.text.textPrimary,
+          }}
+        >
+          {noCourseText}
+        </Text>
+        <GoGetCourseButton />
+      </View>
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: "center";
+`;
+
+export default EmptyTour;
