5d7806e0b3d0471b0e7e11220bf8c495de17b922
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-12-01 14:23:04 +0900

    [#17] feat : 경로 카드 스토어 작성

---
+++ src/store/useRouteCartStore.ts
@@ -0,0 +1,33 @@
+import {create} from "zustand";
+
+import {Point} from "@/types/geom";
+
+interface RouteCartStore {
+  routeCartItems: RouteCartItem[];
+  insertRouteCartItem: (routeCartItem: RouteCartItem) => void;
+  removeRouteCartItem: (routeCartItemId: string) => void;
+}
+
+export interface RouteCartItem {
+  sightId: string; // id
+  theme: string; // 관광지 테마
+  title: string; // 관광지명
+  address: string; // 구/동 단위 주소
+  point: Point; // 위경도
+  imageUrl: string;
+}
+
+export const useRouteCartStore = create<RouteCartStore>((set, get) => ({
+  routeCartItems: [],
+
+  insertRouteCartItem: (routeCartItem: RouteCartItem): void => {
+    set(({routeCartItems}) => ({
+      routeCartItems: [...routeCartItems, routeCartItem]
+    }));
+  },
+  removeRouteCartItem: (routeCartItemId: string): void => {
+    get().routeCartItems.filter((routeCartItem) => {
+      return routeCartItem.sightId !== routeCartItemId;
+    });
+  },
+}));
