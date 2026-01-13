5730d87705ec0a342690ec2f1a01c016474b24ce
Imdahee <ldhdh1832@gmail.com>
2026-01-13 15:55:47 +0900

    [#7]fix: 검색 위경도 한국->서울로 수정

--- src/constants/geometry.ts
+++ src/constants/geometry.ts
@@ -1,4 +1,4 @@
 export const SEOUL_GEOM = {
-  LOGITUDE: { MAX: 133, MIN: 124 },
-  LATITUDE: { MAX: 39, MIN: 33 },
+  LOGITUDE: { MAX: 127.2, MIN: 126.73 },
+  LATITUDE: { MAX: 37.7, MIN: 37.41 },
 };
